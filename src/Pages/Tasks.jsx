f789909c330054ad0e90bfcc6eeb4bd4453ed0e8
Itzmeyogesh <maskeyogeish@gmail.com>
2025-04-18 14:03:08 +0530

    Fix: removed incompatible react-beautiful-dnd, added hello-pangea

--- src/Pages/Tasks.jsx
+++ src/Pages/Tasks.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+
 
 const initialTasks = [
   { id: "1", content: "Complete React project" },
